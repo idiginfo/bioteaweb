cd854b9fab8021303da69fdbeb190deb7236be31
Casey McLaughlin <caseyamcl@gmail.com>
2012-11-06 12:59:58 -0500

    Reworked the DocsClear to work in batches

--- api/src/Bioteawebapi/Commands/DocsClear.php
+++ api/src/Bioteawebapi/Commands/DocsClear.php
@@ -17,6 +17,11 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Doctrine\DBAL\Schema\Table;
+
+use TaskTracker\Tracker;
+use TaskTracker\OutputHandler\SymfonyConsole as TrackerConsoleHandler;
+use TaskTracker\OutputHandler\Monolog as TrackerMonologHandler;
 
 /**
  * Clear all documents in the system
@@ -25,9 +30,12 @@ class DocsClear extends Command
 {
     const NO_LIMIT = 0;
 
+    // --------------------------------------------------------------
+
     protected function configure()
     {
         $this->setName('docs:clear')->setDescription('Clear all documents in the system');
+        $this->addOption('batchsize', 'b', InputOption::VALUE_REQUIRED, 'Batch Size (default 10,000)', 10000);
     }
 
     // --------------------------------------------------------------
@@ -45,15 +53,18 @@ class DocsClear extends Command
             }
         }
 
-        //Get schema manager and database clients
-        $db = $this->app['db'];
+        //Get schema manager
         $sm = $this->app['db']->getSchemaManager();
+
+        //Get batch size
+        $batchSize = $input->getOption('batchsize');
 
         //Foreach table, build delete statement
         $stmnts = array();
         foreach($sm->listTables() as $table) {
-            $tname     = $table->getName();
-            $stmnts[$tname] = $db->prepare("DELETE FROM {$tname};");
+
+            $this->deleteFromTable($table, $batchSize, $output);
+
         }
 
         //Do it
@@ -61,6 +72,62 @@ class DocsClear extends Command
             $output->writeln("Deleting contents of " . $tname . "...");
             $stmt->execute();
         }
+    }
+
+    // --------------------------------------------------------------
+
+    /**
+     * Delete from a table
+     *
+     * @param Doctrine\DBAL\Schema\Table $table
+     * @param int $batchSize
+     * @param OutputInterface $output
+     */
+    protected function deleteFromTable(Table $table, $batchSize, OutputInterface $output)
+    {
+
+        //Get table name 
+        $tname = $table->getName();
+
+        //Get DBAL\Connection object
+        $db = $this->app['db'];
+
+        //Holds total number of deleted records for this table
+        $totalCount = 0;
+
+        //Get the key name
+        $keyName = $db->executeQuery("SHOW KEYS FROM {$tname} WHERE Key_name = 'PRIMARY';")->fetchColumn(4);
+
+        $sq = sprintf('SELECT count(%s) AS count FROM %s', $keyName, $tname);
+        $dq = sprintf('DELETE FROM %s LIMIT %s', $tname, $batchSize);
+
+        //Total number of rows to be deleted
+        $totalNumRows = $db->executeQuery($sq)->fetchColumn(0);
+
+        //Setup a tasktracker
+        $tracker = new Tracker(new TrackerConsoleHandler($output), $totalNumRows);
+        $tracker->start("Starting {$tname}");
+
+        //Delete in chunks
+        while($db->executeQuery($sq)->fetchColumn(0) > 0) {
+
+            //Do it
+            $rows = $db->executeQuery($dq)->rowCount();
+
+            //Reporting
+            $totalCount += $rows;
+            $tracker->tick(sprintf(
+                "Deleting %s rows from %s in batches of %s [%s]",
+                number_format($totalNumRows, 0),
+                $tname,
+                number_format($batchSize, 0),
+                number_format($totalCount, 0)
+            ));
+        }
+
+        $tracker->finish("Done with table {$tname}");
+
+        return $totalCount;
     }
 }
 
